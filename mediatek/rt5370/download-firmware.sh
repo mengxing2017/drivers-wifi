e1c1e268531ad227ca0a1c2c855dc94b5141e268
James Robinson <jamesr@google.com>
2017-03-07 11:08:00 -0800

    Make download-firmware.sh work when invoked from different directory
    Change-Id: I9808ee84d61bc494730e3e8fab57520445ae773a

--- mediatek/rt5370/download-firmware.sh
+++ mediatek/rt5370/download-firmware.sh
@@ -4,5 +4,8 @@
 
 set -e
 
+readonly SCRIPT_ROOT="$(cd $(dirname ${BASH_SOURCE[0]} ) && pwd)"
+cd "${SCRIPT_ROOT}"
+
 curl -o rt2870.zip https://fuchsia-build.storage.googleapis.com/firmware/ralink/$(cat rt2870.sha1)
 unzip -o rt2870.zip rt2870.bin
